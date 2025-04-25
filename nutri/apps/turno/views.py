fed83e6deeb3ccb90e464690e12a148eb5f2bbe4
Mqqqxi <maximilesi20@gmail.com>
2025-04-24 22:49:26 -0300

    asdsadasdaszxczx

--- nutri/apps/turno/views.py
+++ nutri/apps/turno/views.py
@@ -6,6 +6,7 @@ from .models import Turno
 from apps.pacientes.models import Paciente
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+@login_required
 def Turnos(request):
 
 	return render(request, 'turnos/turnos.html')
